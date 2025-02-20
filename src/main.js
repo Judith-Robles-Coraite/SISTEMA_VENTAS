cf527864556db2d3c37216041576d4ace6643619
judith <roblesjudith15@gmail.com>
2025-02-20 14:14:11 -0400

    cambios en packaje.json

--- src/main.js
+++ src/main.js
@@ -1,5 +1,28 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
-createApp(App).mount('#app')
+let mainWindow;
+
+app.whenReady().then(() => {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: true,
+    },
+  });
+
+  // Cargar la aplicación Vue generada por Vite
+  mainWindow.loadURL("http://localhost:5173"); 
+
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
